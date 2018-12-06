5c6918752d42a9bb1069c95d053958a10076df7c
unknown <anshitavishwakarma098@gmail.com>
2018-12-06 22:51:22 +0530

    changes made in app.js

--- react-metronome/src/App.js
+++ react-metronome/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      playing:false,
+      bpm:100,
+    };
+    this.handleChange=this.handleChange.bind(this);
+  }
+
+  handleChange=event=>{
+    const bpm= event.target.value;
+    this.setState({bpm});
+  }
+      render() {
+      const {playing,bpm}=this.state;
+      return (
+      <div className="metroname">
+      <div className="bpm-slider">
+      <div>{bpm} BPM</div>
+      <input type="range" min="60" max="240" value={bpm}
+      onChange={this.handleChange}
+      />
+      </div>
+      <button>{playing?'stop':'start'}</button>
       </div>
     );
   }
